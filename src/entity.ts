e65065477728b9b516b21eb8d0353eccb8348462
Steve Cook <scook@giveinteractive.com>
2024-06-24 10:04:51 -0400

    Update Contact Total Giving

--- src/entity.ts
+++ src/entity.ts
@@ -87,7 +87,6 @@ export class Entity {
   private async _init() {
     try {
       this._token = await new Auth().getToken();
-      console.log('Token', this._token);
     } catch (error) {
       throw error;
     }
